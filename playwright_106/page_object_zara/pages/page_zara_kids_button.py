75913e21247403bf594c648593d9d17fef3ef365
ofril <ofrilev6396@gmail.com>
2026-04-28 12:50:44 +0300

    page object zara

--- playwright_106/page_object_zara/pages/page_zara_kids_button.py
+++ playwright_106/page_object_zara/pages/page_zara_kids_button.py
@@ -6,7 +6,5 @@ class PageZara_kids_button ():
 
     def test_zara_kids_button(self):
 
-        self.page.goto("https://www.zara.com/il/en/search/home")
-        kids_button = self.page.locator('[class="search-sections-column__section"]')
         kids_button = self.page.get_by_role("button, name=kids")
         assert "kids" in self.page.url.lower()
